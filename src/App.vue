77ee8a0d51e163b21118eef0f1b43ccea99c2c33
Mustafa777Xo <mustafa777bdo@gmail.com>
2023-02-07 02:22:20 +0200

    sec commite

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
-  <!-- <Article/> -->
-  <!-- <Audio /> -->
   <router-view></router-view>
 </template>
 <script>
-import Article from './views/Article.vue';
-import Audio from './views/Audio.vue';
-
 export default {
-components:{Article,Audio}
 }
 </script>
 <style >
